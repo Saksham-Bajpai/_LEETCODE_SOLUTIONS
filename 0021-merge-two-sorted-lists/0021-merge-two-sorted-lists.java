f5b07fce2f149a942f143b0da7f9313226884997
Saksham-Bajpai <112783164+Saksham-Bajpai@users.noreply.github.com>
2023-04-20 00:46:33 +0530

    Time: 0 ms (100.00%), Space: 42 MB (70.44%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -14,23 +14,24 @@ class Solution {
         if (list2 == null) return list1;
         
         if (list1.val > list2.val) {
+            //swapping
             ListNode temp = list1;
             list1 = list2;
             list2 = temp;
         }
-        ListNode res = list1;
+        ListNode res = list1; //to store head
         while (list1 != null && list2 != null) {
-            ListNode temp = null;
+            ListNode temp = null; 
             while (list1 != null && list1.val <= list2.val) {
                 temp = list1;
                 list1 = list1.next;
             }
             temp.next = list2;
             
-            //swapping
-            ListNode tmp = list1;
+            //swapping2
+            ListNode temp2 = list1;
             list1 = list2;
-            list2 = tmp;
+            list2 = temp2;
         }
         return res;
     }
